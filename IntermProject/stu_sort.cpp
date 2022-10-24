7be74aac866f233a795ddfec26cea25f41f228d9
DerrickLo6 <116343585+DerrickLo6@users.noreply.github.com>
2022-10-24 02:37:00 -0700

    Update stu_sort.cpp
    I just implemented stu_sort with a simple bubble sort for now

--- IntermProject/stu_sort.cpp
+++ IntermProject/stu_sort.cpp
@@ -10,10 +10,43 @@ stu_sort::stu_sort() {
 
 void d_firstName_sort(DomesticStudent s[]) {
     cout << "First name sort...\n";
+    for(int i = 0; i < 99; i++)
+    {
+        for(int j = i+1; j < 100; j++)
+        {
+            DomesticStudent temp;
+            if(s[i].firstName > s[j].firstName)
+            {
+                //If number or string is bigger than the next then replace s[i] with s[j]
+                //Since temp beforehand is s[i], swap by making s[j] temp now
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+
 }
 
 void d_lastName_sort(DomesticStudent s[]) {
     cout << "Last name sort...\n";
+
+    for(int i = 0; i < 99; i++)
+    {
+
+        for(int j = i+1; j < 100; j++)
+        {
+            DomesticStudent temp;
+            if(s[i].lastName > s[j].lastName)
+            {
+                //If number or string is bigger than the next then replace s[i] with s[j]
+                //Since temp beforehand is s[i], swap by making s[j] temp now
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
 }
 
 void d_cgpa_sort(DomesticStudent s[]) {
@@ -49,10 +82,42 @@ void d_cgpa_sort(DomesticStudent s[]) {
         }
         pos++;
     }*/
+    for(int i = 0; i < 99; i++)
+    {
+        for(int j = i+1; j < 100; j++)
+        {
+            DomesticStudent temp;
+            if(s[i].CGPA < s[j].CGPA)
+            {
+                //If number or string is bigger than the next then replace s[i] with s[j]
+                //Since temp beforehand is s[i], swap by making s[j] temp now
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+
+
 }
 
 void d_researchScore_sort(DomesticStudent s[]) {
     cout << "Research score sort...\n";
+        for(int i = 0; i < 99; i++)
+    {
+        for(int j = i+1; j < 100; j++)
+        {
+            DomesticStudent temp;
+            if(s[i].researchScore < s[j].researchScore)
+            {
+                //If number or string is bigger than the next then replace s[i] with s[j]
+                //Since temp beforehand is s[i], swap by making s[j] temp now
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
 }
 
 void d_sort_all(DomesticStudent s[]) {
@@ -63,18 +128,87 @@ void d_sort_all(DomesticStudent s[]) {
 
 void i_firstName_sort(InternationalStudent s[]) {
     cout << "First name sort...\n";
+        for(int i = 0; i < 99; i++)
+    {
+        for(int j = i+1; j < 100; j++)
+        {
+            //Create temporary local constructor
+            InternationalStudent temp;
+            //Conditional
+            if(s[i].firstName > s[j].firstName)
+            {
+                //If number or string is bigger than the next then replace s[i] with s[j]
+                //Since temp beforehand is s[i], swap by making s[j] temp now
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
 }
 
 void i_lastName_sort(InternationalStudent s[]) {
     cout << "Last name sort...\n";
+        for(int i = 0; i < 99; i++)
+    {
+
+        for(int j = i+1; j < 100; j++)
+        {
+            //Create temporary local constructor
+            InternationalStudent temp;
+            //Conditional
+            if(s[i].lastName > s[j].lastName)
+            {
+                //If number or string is bigger than the next then replace s[i] with s[j]
+                //Since temp beforehand is s[i], swap by making s[j] temp now
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
 }
 
 void i_cgpa_sort(InternationalStudent s[]) {
     cout << "CGPA sort...\n";
+        for(int i = 0; i < 99; i++)
+    {
+        for(int j = i+1; j < 100; j++)
+        {
+            //Create temporary local constructor
+            InternationalStudent temp;
+            //Conditional
+            if(s[i].CGPA < s[j].CGPA)
+            {   
+                //If number or string is bigger than the next then replace s[i] with s[j]
+                //Since temp beforehand is s[i], swap by making s[j] temp now
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
 }
 
 void i_researchScore_sort(InternationalStudent s[]) {
     cout << "Research score sort...\n";
+    for(int i = 0; i < 99; i++)
+    {
+        for(int j = i+1; j < 100; j++)
+        {
+            //Create temporary local constructor
+            InternationalStudent temp;
+            //Conditional
+            if(s[i].researchScore < s[j].researchScore)
+            {
+                //If number or string is bigger than the next then replace s[i] with s[j]
+                //Since temp beforehand is s[i], swap by making s[j] temp now
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
 }
 
 void i_sort_all(InternationalStudent s[]) {
