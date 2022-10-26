d9c320cc4e67f7b7d7abc81bb2024333f8c641b0
amaanraz <amaanraz115@gmail.com>
2022-10-25 19:46:30 -0700

    Internation Overall Sort + main update
    Internation overall sort by cgpa + country (dropping students with low toefl score)
    main menu update

---
+++ stu_sort.hpp
@@ -0,0 +1,18 @@
+//header file stu_sort.hpp to declare your sorting functions
+#ifndef Stu_Sort_HG
+#define Stu_Sort_HG
+
+#include <iostream>
+#include "student.hpp"
+#include <string> //you will have to use string in C++
+#include <vector>
+
+using namespace std; //use namespace std
+
+// Functions
+
+void sortDomestic(DomesticStudent s[], string sorted);
+void sortInternational(InternationalStudent s[], string sorted);
+
+
+#endif
